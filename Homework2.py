b28f0a84d8d48fd783c7684cd39f4574b1da4231
Sunktum0 <vlad99-2010@yandex.ru>
2024-07-14 13:55:36 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,6 @@
+Number_of_completed_homework = 12
+Number_of_hours = 1.5
+Name = 'Python'
+time_one_task = '0,121667'
+print ('Курс:',Name,',','всего задач:',Number_of_completed_homework,',затрачено часов: ',Number_of_hours,',', 'среднее время выаполнения',
+time_one_task, 'часа.')
